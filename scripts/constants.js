6edc6a03ee105e655c908f74389eae92773635d0
dcsimps2 <dcsimps2@illinois.edu>
2017-08-30 17:55:03 -0500

    Update constants.js

--- scripts/constants.js
+++ scripts/constants.js
@@ -6,7 +6,7 @@ window.gauntface = window.gauntface || {};
 window.gauntface.CONSTANTS = {
   GCM_API_KEY: 'AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ',
   APPLICATION_KEYS: {
-    publicKey: 'BDd3_hVL9fZi9Ybo2UUzA284WG5FZR30_95YeZJsiA' + 'pwXKpNcF1rRPF3foIiBHXRdJI2Qhumhf6_LFTeZaNndIo',
-    privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI'
+    publicKey: 'BKa5mtk7zcNOQyyTWLCbcebCcpPOsRhQYvTcHKklzLw8KiQIaiZc_OgcZlx_wfWIa-6_8JEGSgbU1nSQ0KO5Ik0',
+    privateKey: 'xKZKYRNdFFn8iQIF2MH54KTfUHwH105zBdzMR7SI3xI' // bad key
   }
 };
